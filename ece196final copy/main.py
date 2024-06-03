1c094d19364250c59706b4ad8ae0c78901dd94a1
Jazzg3 <91282168+Jazzg3@users.noreply.github.com>
2024-06-02 17:10:47 -0700

    aaaaa

--- ece196final copy/main.py
+++ ece196final copy/main.py
@@ -9,6 +9,28 @@ Created on Thu Oct 13 09:09:45 2022
 import serial # the PySerial library
 import time # for timing purposes
 
+import joblib
+
+# try:
+model = joblib.load('Final_Model.joblib')
+# except Exception as e:
+    # print(f"Error loading model: {e}")
+# model.eval()
+
+# importing the pygame library 
+import pygame 
+import pygame.camera 
+
+# initializing  the camera 
+pygame.camera.init()
+# make the list of all available cameras 
+camlist = pygame.camera.list_cameras() 
+# initializing the cam variable with default camera 
+cam = pygame.camera.Camera(camlist[0], (960, 960), "RGB") 
+# opening the camera 
+cam.start() 
+
+
 def setup(serial_name, baud_rate):
     ser = serial.Serial(serial_name, baudrate=baud_rate)
     return ser
@@ -37,6 +59,23 @@ def main():
     while message == None:
         test = "Arduino test case #1"
         sendable = test[i]
+        
+        image = cam.get_image() 
+        brighten = 40
+        image.fill((brighten, brighten, brighten), special_flags=pygame.BLEND_RGB_ADD)
+        
+        # saving the image to SAME FOLDER as code
+        pygame.image.save(image, "pic%s.jpg" % i)
+        
+        prefix_name = "pic"
+        file_suffix = 0
+        
+        file_name = str(prefix_name + 'file_suffix')
+
+        file_suffix += 1
+
+        sendable = model.predict(file_name + '.jpg')
+        print(sendable)
         
         send_message(ser, sendable)
         time.sleep(1)
